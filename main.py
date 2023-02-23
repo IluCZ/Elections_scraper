21ce7cf18e62f5fcaa7d8f09f10fce671ceeb811
Richard_Cibere <richard.cibere.it@gmail.com>
2023-02-23 20:09:03 +0100

    aa

---
+++ main.py
@@ -0,0 +1,8 @@
+import requests
+import sys
+import bs4
+# Please enter the commit message for your changes. Lines startinga
+print(sys.argv)
+
+if len(sys.argv) != 3:
+    print("Zadej tři argumenty")
